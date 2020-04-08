3ec23e1102a0a206ebe5d132664db40de47a9881
fh.proger <phillipe.souza@rte.com.br>
2020-04-08 14:26:55 -0300

    Habilitando o cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 // iniciando o app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true, useUnifiedTopology: true});
