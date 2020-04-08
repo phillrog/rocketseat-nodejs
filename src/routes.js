2e9924fa01913b6be4cbbb9f4358a12a7f927d41
fh.proger <phillipe.souza@rte.com.br>
2020-04-08 14:14:54 -0300

    CRUD

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,9 @@ const routes = express.Router();
 const ProductController = require('./controller/ProductController');
 
 routes.get('/products', (req, res) => ProductController.index(req, res));
+routes.get('/products/:id', (req, res) => ProductController.show(req, res));
 routes.post('/products', (req, res) => ProductController.store(req, res));
+routes.put('/products/:id', (req, res) => ProductController.update(req, res));
+routes.delete('/products/:id', (req, res) => ProductController.destroy(req, res));
 
 module.exports = routes;
