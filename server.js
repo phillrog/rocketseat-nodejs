815e0738cfd719a6e8078fd45c3ab58fb78e19fd
fh.proger <phillipe.souza@rte.com.br>
2020-04-08 12:23:18 -0300

    Configurando o mongodb

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongooe = require('mongoose');
 
+// iniciando o app
 const app = express();
 
+// iniciando o DB
+mongooe.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true, useUnifiedTopology: true});
+
+// rotas
 app.get('/', (req, res) => {
     res.send('Olá mundo')
 });
